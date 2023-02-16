5e595906ed0e32db0a80145bb98f23ac84115d04
Cesar <c.augusto_mendez@hotmail.com>
2023-02-15 22:24:26 -0400

    Agregando operacion de multiplicar

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,15 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+
+const third = document.querySelector("#tercer-numero");
+const fourth = document.querySelector("#cuarto-numero");
+const form_m = document.querySelector("#multiplicar-form");
+const div_m = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,4 +18,13 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+form_m.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const thirdNumber = Number.parseInt(third.value);
+  const fourthNumber = Number.parseInt(fourth.value);
+
+  div.innerHTML = "<p>" + multiplicar(thirdNumber, fourthNumber) + "</p>";
 });
