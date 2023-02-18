f4b979017bce76a355324fd21cc986268445af77
Cesar <c.augusto_mendez@hotmail.com>
2023-02-18 16:06:39 -0400

    Implementando la opcion de mostrar el impuesto en pantalla

--- src/presenter.js
+++ src/presenter.js
@@ -1,3 +1,5 @@
+import mostrar_impuestos from "./calcular_total";
+
 const total_items = document.querySelector("#cantidad_items");
 const precio_items = document.querySelector("#precio_item");
 const estados = document.querySelector("#estados");
@@ -5,8 +7,16 @@ const estados = document.querySelector("#estados");
 const form = document.querySelector("#totalizador_form");
 const div = document.querySelector("#resultado-div");
 
+const impuestos = new Map([
+    ['UT', 6.65],
+    ['NV', 8],
+    ['TX', 6.25],
+    ['AL', 4],
+    ['CA', 8.25]
+  ]);
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  div.innerHTML = "<p>" + "La cant de items es: " +total_items.value + " y el precio unitario es: "+ precio_items.value + " el estado selecionado es: " + estados.value + "<p>";
+  div.innerHTML = "<p>" + "La cant de items es: " +total_items.value + " y el precio unitario es: "+ precio_items.value + " el estado selecionado es: " + estados.value + " el impuesto es" + mostrar_impuestos(impuestos,estados.value) + "<p>";
 });
