fd9f9c7c003459ddbd86790679fc9826f9a96e30
Cesar <c.augusto_mendez@hotmail.com>
2023-02-18 14:57:56 -0400

    Implementacion de saludo por idiomas espaniol, ingles

--- src/presenter-saludador.js
+++ src/presenter-saludador.js
@@ -4,13 +4,14 @@ const user_gender = document.querySelector("#gender");
 const user_age = document.querySelector("#age");
 const form = document.querySelector("#saludador-form");
 const div = document.querySelector("#resultado-div");
+const idiomas = document.querySelector("#idiomas");
 
-const edad_usuario =Number.parseInt(user_age);
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   let fechaActual = new Date();
   let horaActual = fechaActual.getHours();
 
-  div.innerHTML = verificar(user_name.value, user_age.value, user_gender.value, horaActual);
+
+  div.innerHTML = "<p>" + verificar(user_name.value, user_age.value, user_gender.value, horaActual, idiomas.value) + "<p>";
 });
--- src/verificar_formulario.js
+++ src/verificar_formulario.js
@@ -1,30 +1,71 @@
-function verificar(name, age, gender, hour) {
-    let horario = " ";
-    if(hour >= 1 && hour < 12)    {
-        horario = "Buenos Dias";
-    }
-    else if (hour > 12 && hour < 20){
-        horario = "Buenos Tardes";
+function verificarIdiomas(idioma){
+    idioma_seleccionado = " ";
+    if(idioma == "ES"){
+        idioma_seleccionado = "esp";
     }
     else{
-        horario = "Buenos Noches";
+        idioma_seleccionado = "ing";
     }
+    return idioma_seleccionado;
+}
 
-    if (gender == "M") {
-        if (age > 30) {
-          return horario + " Señor " + name;
-        } 
-        else {
-          return horario + " Joven " + name;
+function verificar(name, age, gender, hour, language) {
+    let horario = " ";
+    if (verificarIdiomas(language) == "esp"){
+        if(hour >= 1 && hour < 12)    {
+            horario = "Buenas Dias";
         }
-      } 
-      else if (gender == "F") {
-        if (age > 30) {
-          return horario + " Señora " + name;
-        } 
-        else {
-          return horario + " Jovencita " + name;
+        else if (hour > 12 && hour < 20){
+            horario = "Buenas Tardes";
         }
-      }
+        else{
+            horario = "Buenas Noches";
+        }
+    
+        if (gender == "M") {
+            if (age > 30) {
+              return horario + " Señor " + name;
+            } 
+            else {
+              return horario + " Joven " + name;
+            }
+          } 
+          else if (gender == "F") {
+            if (age > 30) {
+              return horario + " Señora " + name;
+            } 
+            else {
+              return horario + " Jovencita " + name;
+            }
+          }
+    }else{
+        if(hour >= 1 && hour < 12)    {
+            horario = "Good Morning";
+        }
+        else if (hour > 12 && hour < 20){
+            horario = "Good Afternoon";
+        }
+        else{
+            horario = "Good Night";
+        }
+    
+        if (gender == "M") {
+            if (age > 30) {
+              return horario + " Mr. " + name;
+            } 
+            else {
+              return horario + " "+ name;
+            }
+          } 
+          else if (gender == "F") {
+            if (age > 30) {
+              return horario + " Miss " + name;
+            } 
+            else {
+              return horario + " " + name;
+            }
+          }
+    }
+    
   }
 export default verificar;
