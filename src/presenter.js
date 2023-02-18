94e1bd1855e3b1ec9b617342ce51d107b8e54a0a
Cesar <c.augusto_mendez@hotmail.com>
2023-02-18 15:50:46 -0400

    Implementando la opcion de mostrar el estado en pantalla

--- src/presenter.js
+++ src/presenter.js
@@ -1,5 +1,6 @@
 const total_items = document.querySelector("#cantidad_items");
 const precio_items = document.querySelector("#precio_item");
+const estados = document.querySelector("#estados");
 
 const form = document.querySelector("#totalizador_form");
 const div = document.querySelector("#resultado-div");
@@ -7,5 +8,5 @@ const div = document.querySelector("#resultado-div");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  div.innerHTML = "<p>" + "La cant de items es: " +total_items.value + " y el precio unitario es: "+ precio_items.value + "<p>";
+  div.innerHTML = "<p>" + "La cant de items es: " +total_items.value + " y el precio unitario es: "+ precio_items.value + " el estado selecionado es: " + estados.value + "<p>";
 });
